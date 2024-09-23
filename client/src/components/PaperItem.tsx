89e2934b1c6638797acd8b5ce0e15971a9977f12
thoknu <144660048+thoknu@users.noreply.github.com>
2024-09-23 10:56:14 +0200

    added boiler plate for items

---
+++ client/src/components/PaperItem.tsx
@@ -0,0 +1,18 @@
+﻿export default function PaperItem() {
+
+
+    return (
+        <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
+            <div className="carousel-item">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+                    className="rounded-box"/>
+            </div>
+            <div className="carousel-item">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+                    className="rounded-box"/>
+            </div>
+        </div>
+    );
+}
