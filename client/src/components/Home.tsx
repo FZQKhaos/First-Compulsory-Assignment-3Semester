00592494d6af15f13ccf3c7417f5c4c7be5ed827
FZQKhaos <casjen01@easv365.dk>
2024-09-23 12:32:48 +0200

    Add Drawer

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,24 +1,65 @@
 import React, {useEffect} from "react";
 import {useAtom} from "jotai";
-import {PatientsAtom} from "../atoms/PatientsAtom.tsx";
-import {useInitializeData} from "../useInitializeData.ts";
+import Shoppingcart from '../assests/images/Shoppingcart.png';
+import Customer from '../assests/images/Customer.png';
 
 export default function Home() {
 
-    const [, setProducts] = useAtom(PatientsAtom);
 
     useEffect(() => {
         
     },[])
-    
-    useInitializeData();
+
 
     return (
         <div>
-            <h1 className="menu-title text-5xl m-5">Welcome to Dunder Mifflin Infinity</h1>
-            <h2 className="menu-title text-3xl m-5">Paper Shop</h2>
-
-            
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl">Dunder Mifflin Infinity</a>
+                </div>
+                <div className="flex-none">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <div className="indicator">
+                                <img src= {Shoppingcart} />
+                                <span className="badge badge-sm indicator-item">8</span>
+                            </div>
+                        </div>
+                        <div
+                            tabIndex={0}
+                            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+                            <div className="card-body">
+                                <span className="text-lg font-bold">8 Items</span>
+                                <span className="text-info">Subtotal: $999</span>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary btn-block">View cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src={Customer}/>
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
