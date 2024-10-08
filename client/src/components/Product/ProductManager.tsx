7ca1e71fd25ec574d10eac2751ad60c494555243
FZQKhaos <casjen01@easv365.dk>
2024-10-08 14:43:51 +0200

    Possible to add more stock to papers

--- client/src/components/Product/ProductManager.tsx
+++ client/src/components/Product/ProductManager.tsx
@@ -1,13 +1,16 @@
 import CreatePaper from "./CreatePaper.tsx";
 import DiscontinuePaper from "./DiscontinuePaper.tsx";
+import ProductStock from "./ProductStock.tsx";
 
 export default function ProductManager () {
 
     return (
-        <div className="flex w-3/4 mx-auto justify-center mt-5">
+        <div className="flex w-5/6 mx-auto justify-center mt-5">
             <CreatePaper />
             <div className="divider divider-horizontal"></div>
             <DiscontinuePaper />
+            <div className="divider divider-horizontal"></div>
+            <ProductStock />
         </div>
     )
 }
